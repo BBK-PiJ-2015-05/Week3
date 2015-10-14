2500754010b1fc26df511dc52a664c8b5b529bf6
cannedfruit <sarah.a.connor@gmail.com>
2015-10-14 10:59:49 +0100

    Week 3 Day 6 1. Dividing integers

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+	
+	/*public Calculator(){
+		super();
+	} */
+	
+	
+	public static int add(int a, int b){
+		return a + b;
+	}
+	
+	public static int subtract(int a, int b){
+		return a - b;
+	}
+	
+	public static int multiply(int a, int b){
+		return a * b;
+	}
+	
+	public static int divide(int a, int b){
+		return a / b;
+	}
+	
+	public static int modulus(int a, int b){
+		return a % b;
+	}
+
+}
